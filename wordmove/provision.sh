c56a04f5fe4d5b8513cb6a18bcfcd717778bd1fa
Francisco J. Godoy <hola@franciscogodoy.es>
2019-04-05 00:54:04 +0200

    Testing code WIP

--- wordmove/provision.sh
+++ wordmove/provision.sh
@@ -11,25 +11,25 @@ else
 fi
 
 # wordmove install
-wordmove_install="$(gem list wordmove -i)"
-if [ "$wordmove_install" = true ]; then
-  echo "wordmove installed"
-else
-  echo "wordmove not installed"
-  sudo gem install wordmove
+# wordmove_install="$(gem list wordmove -i)"
+# if [ "$wordmove_install" = true ]; then
+#   echo "wordmove installed"
+# else
+#   echo "wordmove not installed"
+#   sudo gem install wordmove
 
-  wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
-  if [  "$(grep yaml $wordmove_path)" ]; then
+#   wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
+#   if [  "$(grep yaml $wordmove_path)" ]; then
 
 
-    echo "can require yaml"
-  else
-    echo "can't require yaml"
-    echo "set require yaml"
+#     echo "can require yaml"
+#   else
+#     echo "can't require yaml"
+#     echo "set require yaml"
 
-    sudo sed -i "7i require\ \'yaml\'" $wordmove_path
+#     sudo sed -i "7i require\ \'yaml\'" $wordmove_path
 
-    echo "can require yaml"
+#     echo "can require yaml"
 
-  fi
-fi
+#   fi
+# fi
