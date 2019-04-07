deb3bac71d46d9cd7f86ecee4e9520dc283f5ba1
Francisco J. Godoy <hola@franciscogodoy.es>
2019-04-07 14:41:57 +0200

    Remove 'sudo gem update --system'

--- wordmove4/provision.sh
+++ wordmove4/provision.sh
@@ -5,12 +5,11 @@ if [ $(gem -v|grep '^2.') ]; then
 else
 	sudo apt-add-repository -y ppa:brightbox/ruby-ng
 	sudo apt-get update
-	sudo apt-get install -y ruby2.6-dev
+	sudo apt-get install -y ruby2.6-full
 	echo "ruby-dev installed"
 	echo "gem not installed"
 	sudo gem install rubygems-update
 	sudo update_rubygems
-	sudo gem update --system
 fi
 
 # wordmove install
