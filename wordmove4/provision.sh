e6dfc4a8b9ace117e40d22e28716f5f5cb3a455a
Francisco Godoy <hola@franciscogodoy.es>
2019-04-07 21:06:02 +0200

    Installing 'ruby2.3 ruby2.3-dev' packages

--- wordmove4/provision.sh
+++ wordmove4/provision.sh
@@ -5,7 +5,7 @@ if [ $(gem -v|grep '^2.') ]; then
 else
 	sudo apt-add-repository -y ppa:brightbox/ruby-ng
 	sudo apt-get update
-	sudo apt-get install -y ruby2.3-full
+	sudo apt-get install -y ruby2.3 ruby2.3-dev
 	echo "ruby-dev installed"
 	echo "gem not installed"
 	sudo gem install rubygems-update
